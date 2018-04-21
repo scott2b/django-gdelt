eeb0c5996d7c99114cc71fd1cc82cf0fd51459cc
Scott Bradley <scottbradley.mail@gmail.com>
2018-04-21 16:33:43 -0500

    moves classifier into models

--- djdelt/models.py
+++ djdelt/models.py
@@ -75,3 +75,28 @@ class GKGMedia(models.Model):
     url = models.URLField()
     media_type = models.CharField(max_length=18)
     media_type = models.CharField(max_length=18)
+
+
+class Classifier(object):
+
+    @classmethod
+    def classify_text(cls, text):
+        text = text.lower()
+        info = {}
+        for term in PatternTerm.objects.all():
+            if term.term in text:
+                cat = term.category
+                cat_name = term.category.name
+                cat_type = term.category.cat_type
+                if not cat_type in info:
+                    info[cat_type] = {}
+                if not cat_name in info[cat_type]:
+                    info[cat_type][cat_name] = 0
+                info[cat_type][cat_name] += term.score
+        for type_, categories in info.items():
+            collection = { cat:score for cat, score in categories.items() if score > 0 }
+            if collection:
+                info[type_] = collection
+            else:
+                del(info[type_])
+        return info
