dec03a618cd5b4035db455a2a714eb1955182431
Scott Bradley <scottbradley.mail@gmail.com>
2018-04-26 20:58:24 -0500

    adds text field

--- djdelt/models.py
+++ djdelt/models.py
@@ -62,6 +62,7 @@ class GKGDocument(models.Model):
     translation_info = models.TextField(blank=True)
     extras_xml = models.TextField(blank=True)
     html = models.TextField(blank=True)
+    text = models.TextField(blank=True)
 
     def __str__(self):
         return '%s: %s' % (self.gkg_record_id, self.document_identifier)
